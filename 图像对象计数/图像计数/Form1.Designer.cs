5e6b05f9d3513804f7f185a448e0ba775984769b
周晨阳 <1448517833@qq.com>
2021-11-20 14:50:48 +0800

    21.11.20

---
+++ 图像对象计数/图像计数/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace 图像计数
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::图像计数.Properties.Resources._1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 399);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 图像对象计数/图像计数/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 图像计数
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 图像计数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap("C:\\Users\\ZZZCNY\\Documents\\GitHub\\Multimedia-Technology-and-Application\\图像对象计数\\图像计数\\1.bmp");
+            Color color = new Color();
+            int count = 0;
+            int[] count1 = { 0 };
+            int[,] vs = new int[bitmap.Width, bitmap.Height];
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    color = bitmap.GetPixel(x, y);
+                    if (color.R == 0 && color.G == 0 && color.B == 0)
+                    {
+                        if (x - 1 < 0 || y - 1 < 0 || x + 1 == bitmap.Width || y + 1 == bitmap.Height)
+                        {
+                            continue;
+                        }
+                        if (vs[x - 1, y - 1] != 0 || vs[x - 1, y] != 0 || vs[x - 1, y + 1] != 0 || vs[x, y - 1] != 0 || vs[x, y + 1] != 0 || vs[x + 1, y - 1] != 0 || vs[x + 1, y] != 0 || vs[x + 1, y + 1] != 0)
+                        {
+                            if (vs[x, y] != 0)
+                            {
+                                continue;
+                            }
+                            vs[x, y] = 1;
+                            if (bitmap.GetPixel(x + 1, y).R == 0)
+                            {
+                                for (int x1 = x + 1; x1 < bitmap.Width; x1++)
+                                {
+                                    for (int y1 = y; y1 > 0; y1--)
+                                    {
+                                        color = bitmap.GetPixel(x1, y1);
+                                        if (color.R == 0 && color.G == 0 && color.B == 0)
+                                        {
+                                            vs[x1, y1] = 1;
+                                        }
+                                        if (bitmap.GetPixel(x1, y1 - 1).R == 255)
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    for (int y1 = y; y1 < bitmap.Height; y1++)
+                                    {
+                                        color = bitmap.GetPixel(x1, y1);
+                                        if (color.R == 0 && color.G == 0 && color.B == 0)
+                                        {
+                                            vs[x1, y1] = 1;
+                                        }
+                                        if (bitmap.GetPixel(x1, y1 + 1).R == 255)
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    if (bitmap.GetPixel(x1 + 1, y).R == 255)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                            if (bitmap.GetPixel(x - 1, y).R == 0)
+                            {
+                                for (int x1 = x - 1; x1 > 0; x1--)
+                                {
+                                    for (int y1 = y; y1 > 0; y1--)
+                                    {
+                                        color = bitmap.GetPixel(x1, y1);
+                                        if (color.R == 0 && color.G == 0 && color.B == 0)
+                                        {
+                                            vs[x1, y1] = 1;
+                                        }
+                                        if (bitmap.GetPixel(x1, y1 - 1).R == 255)
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    for (int y1 = y; y1 < bitmap.Height; y1++)
+                                    {
+                                        color = bitmap.GetPixel(x1, y1);
+                                        if (color.R == 0 && color.G == 0 && color.B == 0)
+                                        {
+                                            vs[x1, y1] = 1;
+                                        }
+                                        if (bitmap.GetPixel(x1, y1 + 1).R == 255)
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    if (bitmap.GetPixel(x1 - 1, y).R == 255)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            vs[x, y] = 1;
+                            if (bitmap.GetPixel(x + 1, y).R == 0)
+                            {
+                                for (int x1 = x + 1; x1 < bitmap.Width; x1++)
+                                {
+                                    for (int y1 = y; y1 > 0; y1--)
+                                    {
+                                        color = bitmap.GetPixel(x1, y1);
+                                        if (color.R == 0 && color.G == 0 && color.B == 0)
+                                        {
+                                            vs[x1, y1] = 1;
+                                        }
+                                        if (bitmap.GetPixel(x1, y1 - 1).R == 255)
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    for (int y1 = y; y1 < bitmap.Height; y1++)
+                                    {
+                                        color = bitmap.GetPixel(x1, y1);
+                                        if (color.R == 0 && color.G == 0 && color.B == 0)
+                                        {
+                                            vs[x1, y1] = 1;
+                                        }
+                                        if (bitmap.GetPixel(x1, y1 + 1).R == 255)
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    if (bitmap.GetPixel(x1 + 1, y).R == 255)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                            if (bitmap.GetPixel(x - 1, y).R == 0)
+                            {
+                                for (int x1 = x - 1; x1 > 0; x1--)
+                                {
+                                    for (int y1 = y; y1 > 0; y1--)
+                                    {
+                                        color = bitmap.GetPixel(x1, y1);
+                                        if (color.R == 0 && color.G == 0 && color.B == 0)
+                                        {
+                                            vs[x1, y1] = 1;
+                                        }
+                                        if (bitmap.GetPixel(x1, y1 - 1).R == 255)
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    for (int y1 = y; y1 < bitmap.Height; y1++)
+                                    {
+                                        color = bitmap.GetPixel(x1, y1);
+                                        if (color.R == 0 && color.G == 0 && color.B == 0)
+                                        {
+                                            vs[x1, y1] = 1;
+                                        }
+                                        if (bitmap.GetPixel(x1, y1 + 1).R == 255)
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    if (bitmap.GetPixel(x1 - 1, y).R == 255)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                            count++;
+                        }
+                    }
+                }
+            }
+            label1.Text = count.ToString();
+            int x2 = 0, y2 = 0, max = 0, min = 0;
+            count = 0;
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    count = 0;
+                    x2 = 0;
+                    y2 = 0;
+                    if (vs[x, y] != 0)
+                    {
+                        for (int i = 0; i < bitmap.Width; i++)
+                        {
+                            for (int j = 0; j < bitmap.Height; j++)
+                            {
+                                if (vs[i, j] == 1)
+                                {
+                                    if (x2 == 0 && y2 == 0)
+                                    {
+                                        x2 = i;
+                                        y2 = j;
+                                        max = min = j;
+                                        count++;
+                                        vs[i, j] = 0;
+                                    }
+                                    else
+                                    {
+                                        if ((i == x2 + 1 || i == x2) && j <= max + 8 && j >= min - 8)
+                                        {
+                                            x2 = i;
+                                            y2 = j;
+                                            count++;
+                                            vs[i, j] = 0;
+                                            if (max < j)
+                                            {
+                                                max = j;
+                                            }
+                                            if (min > j)
+                                            {
+                                                min = j;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        label1.Text += " " + count.ToString();
+                    }
+                }
+            }
+            bitmap.Dispose();
+        }
+    }
+}
