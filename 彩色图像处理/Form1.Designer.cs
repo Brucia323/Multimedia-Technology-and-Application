358cf9ff51d61f0bbb9c0d23da5e4c341cb05fa2
ZZZCNY <1448517833@qq.com>
2021-12-17 17:48:32 +0800

    21.12.17

---
+++ 彩色图像处理/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace 彩色图像处理
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "马赛克处理";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(166, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "浮雕处理";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(972, 650);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(996, 43);
+            this.flowLayoutPanel1.TabIndex = 5;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoScroll = true;
+            this.flowLayoutPanel2.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 43);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(996, 674);
+            this.flowLayoutPanel2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(996, 717);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+    }
+}
+
---
+++ 彩色图像处理/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 彩色图像处理
+{
+    public partial class Form1 : Form
+    {
+        Point start, end;
+        bool mDown = false;
+        Rectangle rect;
+        Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mDown)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    end = e.Location;
+                    rect.Location = new Point(Math.Min(start.X, end.X), Math.Min(start.Y, end.Y));
+                    rect.Size = new Size(Math.Abs(start.X - end.X), Math.Abs(start.Y - end.Y));
+                    pictureBox1.Invalidate();
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mDown = false;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (mDown)
+            {
+                if (pictureBox1.Image != null)
+                {
+                    if (rect != null && rect.Width > 0 && rect.Height > 0)
+                    {
+                        e.Graphics.DrawRectangle(new Pen(Color.Black), rect);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const int N = 5;
+            int r = 0, g = 0, b = 0;
+            Color color;
+            for (int y = start.Y; y < end.Y; y++)
+            {
+                for (int x = start.X; x < end.X; x++)
+                {
+                    if (y == start.Y && x == start.X)
+                    {
+                        color = bmp.GetPixel(x, y);
+                        r = color.R;
+                        g = color.G;
+                        b = color.B;
+                    }
+                    else if (x == start.X)
+                    {
+                        if (y % N == 0)
+                        {
+                            color = bmp.GetPixel(x, y);
+                            r = color.R;
+                            g = color.G;
+                            b = color.B;
+                        }
+                    }
+                    else if (y % N == 0 || y == start.Y)
+                    {
+                        if (x % N == 0)
+                        {
+                            color = bmp.GetPixel(x, y);
+                            r = color.R;
+                            g = color.G;
+                            b = color.B;
+                        }
+                        else
+                        {
+                            bmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+                        }
+                    }
+                    else
+                    {
+                        Color color1 = bmp.GetPixel(x, y - 1);
+                        bmp.SetPixel(x, y, color1);
+                    }
+                }
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap("C:\\Users\\ZZZCNY\\Documents\\GitHub\\Multimedia-Technology-and-Application\\彩色图像处理\\image\\RE4wqHB.jpg");
+            pictureBox1.Image = bmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int height = rect.Height;
+            int width = rect.Width;
+            Bitmap bitmap = new Bitmap(width, height);
+            Color pixel1, pixel2;
+            for (int x = start.X; x < end.X - 1; x++)
+            {
+                for (int y = start.Y; y < end.Y - 1; y++)
+                {
+                    int r = 0, g = 0, b = 0;
+                    pixel1 = bmp.GetPixel(x, y);
+                    pixel2 = bmp.GetPixel(x + 1, y + 1);
+                    r = Math.Abs(pixel1.R - pixel2.R + 128);
+                    g = Math.Abs(pixel1.G - pixel2.G + 128);
+                    b = Math.Abs(pixel1.B - pixel2.B + 128);
+                    if (r > 255)
+                    {
+                        r = 255;
+                    }
+                    if (g > 255)
+                    {
+                        g = 255;
+                    }
+                    if (b > 255)
+                    {
+                        b = 255;
+                    }
+                    if (r < 0)
+                    {
+                        r = 0;
+                    }
+                    if (g < 0)
+                    {
+                        g = 0;
+                    }
+                    if (b < 0)
+                    {
+                        b = 0;
+                    }
+                    bmp.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            start = e.Location;
+            Invalidate();
+            mDown = true;
+        }
+    }
+}
